9616d758d1b8aa9c0486bb9098aa056521d4360f
amul-jos <98321461+amul-jos@users.noreply.github.com>
2024-11-04 20:28:44 +0100

    convert the package to pip installable

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='1.0',
+    packages=['math_quiz'],
+    url='',
+    license='',
+    author='amalna.jose',
+    author_email='amalna.jose@fau.de',
+    description='solve math problems'
+)
